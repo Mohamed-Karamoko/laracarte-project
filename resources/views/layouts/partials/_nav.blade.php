2b46bc00a90a1b3dae6580c0cd9be9d2678e40cb
KARAMOKO Mohamed <mohamed.karamoko70@gmail.com>
2020-08-03 17:06:06 +0200

    Add set_active_route helpers

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-dafault navbar-static-top">
+<nav class="navbar navbar-inverse navbar-static-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="
@@ -12,8 +12,8 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="{{ route('root_path') }}">Home</a></li>
-                <li><a href="{{ route('about_path') }}">About</a></li>
+                <li class="{{ set_active_route('root_path') }}"><a href="{{ route('root_path') }}">Home</a></li>
+                <li class="{{ set_active_route('about_path')}}"><a href="{{ route('about_path') }}">About</a></li>
                 <li><a href="#artisans">Artisans</a></li>
                 <li class="dropdown">
                     <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopop="
